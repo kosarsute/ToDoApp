afe39a5594411f85b5a0b66d16b3363ba91f950a
Sergei Kosarev <89865368+kosarsute@users.noreply.github.com>
2022-07-20 13:19:23 -0700

    DataProvider

--- ToDoApp/Controller/DataProvider.swift
+++ ToDoApp/Controller/DataProvider.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 import UIKit
+enum Section: Int {
+    case todo
+    case done
+}
 class DataProvider: NSObject {
+    var taskManager: TaskManager?
     
 }
 extension DataProvider: UITableViewDelegate {
@@ -15,10 +20,18 @@ extension DataProvider: UITableViewDelegate {
 }
 extension DataProvider: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        guard let section = Section(rawValue: section) else { fatalError() }
+        guard let taskManager = taskManager else  { return 0 }
+        switch section {
+        case .todo: return taskManager.tasksCount
+        case .done: return taskManager.doneTasksCount
+    }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        return TaskCell()
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
 }
---
+++ ToDoApp/View/TaskCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaskCell.swift
+//  ToDoApp
+//
+//  Created by SERGEI KOSAREV on 7/20/22.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+
+}
